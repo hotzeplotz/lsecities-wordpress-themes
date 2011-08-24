72cf73d93a997957e543931a4e32f02d001b1570
andrea rota <a.rota@lse.ac.uk>
2011-08-24 16:36:31 +0100

    added code for second language

--- lsecities-twentyeleven/pods-articles.php
+++ lsecities-twentyeleven/pods-articles.php
@@ -21,19 +21,30 @@
       global $pods;
       /* URI: /media/objects/articles/<article-slug>/<language> */
       $pod = new Pod('article', pods_url_variable(3));
-      $lang = pods_url_variable(4);
-    ?>
+      $lang = strtolower(pods_url_variable(4));
+      $article_lang2 = $pod->get_field('language')[0]['slug'];
 
-    <code class="pods-debug"><?php var_dump($pod->get_field('language')); ?></code>
-    <code class="pods-debug"><?php echo $pod->data; ?></code>
+      echo '<code class="debug">req_lang: ' . $lang . '</code>';
+      echo '<code class="debug">lang2: ' . $article_lang2 . '</code>';
+      
+      if(!empty($lang) && $lang == $article_lang2)) {
+        $article_title = $pod->get_field('title_lang2');
+        $article_abstract = $pod->get_field('abstract_lang2');
+        $article_text = $pod->get_field('text_lang2');
+      } else {
+        $article_title = $pod->get_field('name');
+        $article_abstract = $pod->get_field('abstract');
+        $article_text = $pod->get_field('text');
+      }
+    ?>
     
     <?php if(!empty($pod->data)) : ?>
       <div id="primary">
         <div id="content" role="main">
           <div class="article">
-            <h1 class="entry-title article-title"><?php echo $pod->get_field('name'); ?></h1>
-            <div class="entry-meta article-abstract"><?php echo $pod->get_field('abstract'); ?></div>
-            <div class="entry-content article-text"><?php echo $pod->get_field('text'); ?></div>
+            <h1 class="entry-title article-title"><?php echo $article_title; ?></h1>
+            <div class="entry-meta article-abstract"><?php echo $article_abstract; ?></div>
+            <div class="entry-content article-text"><?php echo $article_text; ?></div>
           </div>
         </div>
       </div>
