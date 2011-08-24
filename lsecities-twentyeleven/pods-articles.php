97eb5498d046480392894b6cf9bb539094ee56f7
andrea rota <a.rota@lse.ac.uk>
2011-08-24 14:04:06 +0100

    added shared page elements

--- lsecities-twentyeleven/pods-articles.php
+++ lsecities-twentyeleven/pods-articles.php
@@ -9,6 +9,8 @@
  */
 ?>
 
+<?php get_header(); ?>
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
@@ -47,3 +49,7 @@
 		<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
 </article><!-- #post-<?php the_ID(); ?> -->
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
