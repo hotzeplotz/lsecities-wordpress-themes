2b39e5e09bde9fdcf6a3935d1204eb5e8bbd5d2a
andrea rota <a.rota@lse.ac.uk>
2011-08-30 15:27:16 +0100

    removed tags from wrong position

--- lsecities-twentyeleven/pods-articles.php
+++ lsecities-twentyeleven/pods-articles.php
@@ -49,15 +49,11 @@
     ?>
     
     <?php if(!empty($pod->data)): ?>
-      <div id="primary">
-        <div id="content" role="main">
-          <div class="article">
-            <h1 class="entry-title article-title"><?php echo $article_title; ?></h1>
-            <div class="entry-meta article-abstract"><?php echo $article_abstract; ?></div>
-            <div class="entry-content article-text"><?php echo $article_text; ?></div>
-            <div class="extra-content"><?php echo $article_extra_content; ?></div>
-          </div>
-        </div>
+      <div class="article">
+        <h1 class="entry-title article-title"><?php echo $article_title; ?></h1>
+        <div class="entry-meta article-abstract"><?php echo $article_abstract; ?></div>
+        <div class="entry-content article-text"><?php echo $article_text; ?></div>
+        <div class="extra-content"><?php echo $article_extra_content; ?></div>
       </div>
     <?php endif; ?>
         
