20a6ffe826d213cb981e826723c08cd2df00edf5
andrea rota <a.rota@lse.ac.uk>
2011-08-30 11:42:54 +0100

    new constant

--- lsecities-twentyeleven/inc/pods-functions.php
+++ lsecities-twentyeleven/inc/pods-functions.php
@@ -3,3 +3,4 @@
 /* LSE Cities Twenty Eleven functions and variable definitions */
 
 $PODS_BASEURI_ARTICLES = '/media/objects/articles';
+$PODS_BASEURI_NEW_ITEM = '/wp-admin/admin.php?page=pods-add-';
