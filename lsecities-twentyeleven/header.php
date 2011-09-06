7be5d1cc3014d178e0817243f01063f8b93850d0
andrea rota <a.rota@lse.ac.uk>
2011-09-06 09:28:40 +0100

    fixed path

--- lsecities-twentyeleven/header.php
+++ lsecities-twentyeleven/header.php
@@ -47,7 +47,7 @@
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-<link rel="stylesheet" type="text/css" media="all" href="stylesheets/style_lc.css" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_directory') ?>/stylesheets/style_lc.css" />
 <link href='http://fonts.googleapis.com/css?family=Gentium+Book+Basic:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>
