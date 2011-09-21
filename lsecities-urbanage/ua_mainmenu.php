1ff7904f08ca8577550b8e70084a351d43f3040e
andrea rota <a.rota@lse.ac.uk>
2011-09-21 08:23:38 +0100

    updated menus

--- lsecities-urbanage/ua_mainmenu.php
+++ lsecities-urbanage/ua_mainmenu.php
@@ -1,89 +1,93 @@
-
 <!-- mainMenu.begin -->
 <div id="mainMenu">
       <ul class="menu">
        <li class="leaf">
-        <a accesskey="h" href="<?php echo $base_uri; ?>/">Home</a>
+        <a href="/">Home</a>
        </li>
         
-      <li class="tree">
-          <span accesskey="a">About</span>
+      <li class="leaf">
+          <a href="/introduction/idea/">About</a>
+        </li>
+
+        <li class="tree">
+          <span class="leaf"><a href="/network/">Who's who</a></span>
 
           <ul class="tree">
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/introduction/idea/">Idea</a></li>
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/introduction/network/">Network</a></li>
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/introduction/conference/">Conference</a></li>
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/introduction/investigation/">Investigation</a></li>
+            <li class="leaf"><a href="/02_network/network_Board.html">Board</a></li>
+            <li class="leaf"><a href="/02_network/network_Staff.html">Staff</a></li>
+            <li class="leaf"><a href="/02_network/network_Advisors.html">Advisors and Contributors</a></li>
+
+            <li class="leaf"><a href="/02_network/network_Institutions.html">Institutional Partners</a></li>    
           </ul>
         </li>
 
         <li class="tree">
-          <span accesskey="n">Network</span>
+          <span class="leaf"><a href="/conferences/">Conferences</a></span>
 
           <ul class="tree">
-            <li class="leaf"><a href="/<?php echo $base_uri; ?>02_network/network_Board.html">Board</a></li>
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/02_network/network_Staff.html">Staff</a></li>
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/02_network/network_Advisors.html">Advisors and Contributors</a></li>
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/02_network/network_Institutions.html">Institutional Partners</a></li>    
+            <li class="leaf"><a href="/03_conferences/conf_newYork.html">New York</a></li>
+
+            <li class="leaf"><a href="/03_conferences/conf_shanghai.html">Shanghai</a></li>
+            <li class="leaf"><a href="/03_conferences/conf_london.html">London</a></li>
+            <li class="leaf"><a href="/03_conferences/conf_mexicoCity.html">Mexico City</a></li>
+            <li class="leaf"><a href="/03_conferences/conf_johannesburg.html">Johannesburg</a></li>
+            <li class="leaf"><a href="/03_conferences/conf_berlin.html">Berlin</a></li>
+            <li class="leaf"><a href="/03_conferences/conf_halle.html">German Cities, Halle</a></li>
+
+            <li class="leaf"><a href="/03_conferences/conf_mumbai.html">Mumbai</a></li>            
+            <li class="leaf"><a href="/03_conferences/conf_saoPaulo.html">S&atilde;o Paulo</a></li>            
+            <li class="leaf"><a href="/conferences/istanbul/">Istanbul</a></li>
+            <li class="leaf"><a href="/conferences/chicago/">Chicago</a></li>
+            <li class="leaf"><a href="/conferences/hongkong/">Hong Kong</a></li>
+          </ul>
+        </li>
+
+        <li class="leaf">
+          <a href="/events/publicLectures/">Events</a>
+        </li>
+
+        <li class="leaf">
+          <a href="/introduction/investigation/">Research</a>
+        </li>
+
+        <li class="tree">
+          <span class="leaf"><a href="/publications/">Publications</a></span>
+
+          <ul class="tree">
+            <li class="leaf"><a href="/publications/newsletter/current/">Newsletter</a></li>
+            <li class="leaf"><a href="/publications/theEndlessCity/">The Endless City</a></li>
+            <li class="leaf"><a href="/publications/living-in-the-endless-city/">Living in the Endless City</a></li>
+
+            <li class="leaf"><a href="/publications/newspapers/">Conference newspapers</a></li>
+            <li class="leaf"><a href="/publications/reports/">Research reports</a></li>  
+            <li class="leaf"><a href="/publications/archive/">Archive</a></li>
           </ul>
         </li>
 
         <li class="tree">
-          <span accesskey="c">Conferences</span>
+          <span class="leaf"><a href="/dbua-award/">Award</a></span>
 
           <ul class="tree">
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/03_conferences/conf_newYork.html">New York</a></li>
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/03_conferences/conf_shanghai.html">Shanghai</a></li>
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/03_conferences/conf_london.html">London</a></li>
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/03_conferences/conf_mexicoCity.html">Mexico City</a></li>
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/03_conferences/conf_johannesburg.html">Johannesburg</a></li>
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/03_conferences/conf_berlin.html">Berlin</a></li>
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/03_conferences/conf_halle.html">German Cities, Halle</a></li>
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/03_conferences/conf_mumbai.html">Mumbai</a></li>            
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/03_conferences/conf_saoPaulo.html">S&atilde;o Paulo</a></li>            
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/conferences/istanbul/">Istanbul</a></li>
-			<li class="leaf"><a href="<?php echo $base_uri; ?>/03_conferences/conf_chicago/">Chicago</a></li>
-          </ul>
-        </li>
-
-        <li class="tree">
-          <span accesskey="p">Publications</span>
-
-          <ul class="tree">
-            <li><a href="<?php echo $base_uri; ?>/E_Bulletin/current/">E-Bulletin</a></li>
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/publications/theEndlessCity/">The Endless City</a></li>
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/publications/reports/">Reports</a></li>  
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/publications/newspapers/">Conference Newspapers</a></li>
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/publications/cityProfiles/">City Profiles</a></li>
-            <li class="leaf"><a href="<?php echo $base_uri; ?>/publications/archive/">Archive</a></li>
-          </ul>
-        </li>
-
-        <li class="tree">
-          <span accesskey="o">Award</span>
-
-          <ul class="tree">
-		    <li class="leaf"><a target="_blank" href="http://www.alfred-herrhausen-society.org/en/1605.html">2010 Mexico City</a></li>
+            <li class="leaf"><a target="_blank" href="http://www.alfred-herrhausen-society.org/en/1605.html">2010 Mexico City</a></li>
             <li class="leaf"><a target="_blank" href="http://www.alfred-herrhausen-society.org/en/uaa_istanbul.html">2009 Istanbul</a></li>
-			<li class="leaf"><a target="_blank" href="http://www.alfred-herrhausen-society.org/en/50.html">2008 S&atilde;o Paulo</a></li>
+            <li class="leaf"><a target="_blank" href="http://www.alfred-herrhausen-society.org/en/50.html">2008 S&atilde;o Paulo</a></li>
             <li class="leaf"><a target="_blank" href="http://www.alfred-herrhausen-society.org/en/52.html">2007 Mumbai</a></li>
           </ul>
+
         </li>
 
-        <li class="tree">
-          <span accesskey="p">Press</span>
-
-          <ul class="tree">
-				<li class="leaf"><a href="<?php echo $base_uri; ?>/press/information/">Information</a></li>
-				<li class="leaf"><a href="<?php echo $base_uri; ?>/press/news/">News</a></li>
-				<li class="leaf"><a href="<?php echo $base_uri; ?>/07_press/press_articleLinks.html">Press Clippings</a></li>
-          </ul>
-        </li>
-		
         <li class="leaf">
-          <a accesskey="l" href="<?php echo $base_uri; ?>/lsecities/">LSE Cities</a>
+         <a href="http://lsecities.net/category/urban-age">News</a>
         </li>
-		
+
+        <li class="leaf">
+          <a href="/press/information/">Press</a>
+        </li>
+
+        <li class="leaf">
+          <a target="_blank" href="http://www2.lse.ac.uk/LSECities/">LSE Cities</a>
+        </li>
+
       </ul>
     </div>
 <!-- mainMenu.end -->
--- lsecities-urbanage/ua_vertnav.php
+++ lsecities-urbanage/ua_vertnav.php
@@ -1,44 +1,17 @@
 <!-- vertnav.begin -->
-<div id="vertNav" class="defaultBox">
- <div id="conferencesAfrica">
-  <h2 class="zoneName">Africa</h2>
-  <ul>
-   <li class="cityName"><a href="/03_conferences/conf_johannesburg.html">Johannesburg</a></li>
-  </ul>
- </div>
- <div id="conferencesChina">
-  <h2 class="zoneName">China</h2>
-  <ul>
-   <li class="cityName"><a href="/03_conferences/conf_shanghai.html">Shanghai</a></li>
-  </ul>
- </div>
- <div id="conferencesEurope">
-  <h2 class="zoneName">Europe</h2>
-  <ul>
-   <li class="cityName"><a href="/03_conferences/conf_berlin.html">Berlin</a></li>
-   <li class="cityName"><a href="/03_conferences/conf_istanbul.html">Istanbul</a></li>
-   <li class="cityName"><a href="/03_conferences/conf_london.html">London</a></li>
-  </ul>
- </div>
- <div id="conferencesIndia">
-  <h2 class="zoneName">India</h2>
-  <ul>
-   <li class="cityName"><a href="/03_conferences/conf_mumbai.html">Mumbai</a></li>
-  </ul>
- </div>
- <div id="conferencesLatinAmerica">
-  <h2 class="zoneName">Latin America</h2>
-  <ul>
-   <li class="cityName"><a href="/03_conferences/conf_mexicoCity.html">Mexico City</a></li>
-   <li class="cityName"><a href="/03_conferences/conf_saoPaulo.html">S&atilde;o Paulo</a></li>
-  </ul>
- </div>
- <div id="conferencesUnitedStates">
-  <h2 class="zoneName">United States</h2>
-  <ul>
-   <li class="cityName"><a href="/03_conferences/conf_newYork.html">New York</a></li>
-   <li class="cityName"><a href="/03_conferences/conf_chicago/">Chicago</a></li>
-  </ul>
- </div>
+<div id="vertNav">
+ <ul>
+  <li class="cityName conferencesUnitedStates"><a href="/03_conferences/conf_newYork.html">New York</a></li>
+  <li class="cityName conferencesChina"><a href="/03_conferences/conf_shanghai.html">Shanghai</a></li>
+  <li class="cityName conferencesEurope"><a href="/03_conferences/conf_london.html">London</a></li>
+  <li class="cityName conferencesLatinAmerica"><a href="/03_conferences/conf_mexicoCity.html">Mexico City</a></li>
+  <li class="cityName conferencesAfrica"><a href="/03_conferences/conf_johannesburg.html">Johannesburg</a></li>
+  <li class="cityName conferencesEurope"><a href="/03_conferences/conf_berlin.html">Berlin</a></li>
+  <li class="cityName conferencesIndia"><a href="/03_conferences/conf_mumbai.html">Mumbai</a></li>
+  <li class="cityName conferencesLatinAmerica"><a href="/03_conferences/conf_saoPaulo.html">S&#227;o Paulo</a></li>
+  <li class="cityName conferencesEurope"><a href="/conferences/istanbul/">Istanbul</a></li>
+  <li class="cityName conferencesUnitedStates"><a href="/conferences/chicago/">Chicago</a></li>
+  <li class="cityName conferencesChina"><a href="/conferences/hongkong/">Hong Kong</a></li>
+ </ul>
 </div>
 <!-- vertnav.end -->
