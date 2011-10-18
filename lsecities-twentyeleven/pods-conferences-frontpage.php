887a449386575ef55a8f80ae7191e9dfc4394569
andrea rota <a.rota@lse.ac.uk>
2011-10-18 16:24:59 +0100

    log to logfile

--- lsecities-twentyeleven/pods-conferences-frontpage.php
+++ lsecities-twentyeleven/pods-conferences-frontpage.php
@@ -54,7 +54,7 @@ $button_links = $pod->get_field('links');
             </div>
             <div class='featureboxes clearfix row'>
               <?php foreach($button_links as $link) : ?>
-              <?php error_log(var_export($link)); ?>
+              <?php error_log(var_export($link), true); ?>
               <div class='featurebox fourcol'>
                 <a href="<?php echo $link['guid'] ; ?>" title="<?php echo $link['post_title'] ; ?>">
                   <h2><?php echo $link['post_title'] ; ?></h2>
