b107939451ed4394e07658a155e2c6c7af408f52
andrea rota <a.rota@lse.ac.uk>
2011-10-18 21:53:33 +0100

    debug code

--- lsecities-twentyeleven/header.php
+++ lsecities-twentyeleven/header.php
@@ -143,7 +143,7 @@ Media Archive
 $ancestors = get_ancestors($post->ID);
 $toplevel_ancestor = array_pop($ancestors);
 error_log('post ID: ' . $post->ID);
-error_log('ancestors: ' . var_dump($ancestors, true));
+error_log('ancestors: ' . var_export($ancestors, true));
 error_log('toplevel_ancestor: ' . $toplevel_ancestor);
 $level2nav = wp_list_pages('child_of=' . $toplevel_ancestor . '&depth=2&sort_column=menu_order&title_li=&echo=0');
 error_log('level2nav: ' . $level2nav);
