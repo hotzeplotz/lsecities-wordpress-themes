ba31c62e05b7cd815c8f2c09249f733518963953
andrea rota <a.rota@lse.ac.uk>
2011-10-19 14:44:09 +0100

    added links

--- lsecities-twentyeleven/pods-conferences-frontpage.php
+++ lsecities-twentyeleven/pods-conferences-frontpage.php
@@ -71,37 +71,37 @@ $button_links = $pod->get_field('links');
               <h2>Urban Age conferences</h2>
               <ul>
                 <li class='cityName conferencesAsia'>
-                  <a href='#'>Hong Kong</a>
+                  <a href='/ua/conferences/2011-hongkong/'>Hong Kong</a>
                 </li>
                 <li class='cityName conferencesNorthAmerica'>
-                  <a href='#'>Chicago</a>
+                  <a href='/ua/conferences/2010-chicago/'>Chicago</a>
                 </li>
                 <li class='cityName conferencesEurope'>
-                  <a href='#'>Istanbul</a>
+                  <a href='/ua/conferences/2009-istanbul/'>Istanbul</a>
                 </li>
                 <li class='cityName conferencesSouthAmerica'>
-                  <a href='#'>São Paulo</a>
+                  <a href='/ua/conferences/2008-sao-paulo/'>São Paulo</a>
                 </li>
                 <li class='cityName conferencesAsia'>
-                  <a href='#'>Mumbai</a>
+                  <a href='/ua/conferences/2007-mumbai/'>Mumbai</a>
                 </li>
                 <li class='cityName conferencesEurope'>
-                  <a href='#'>Berlin</a>
+                  <a href='/ua/conferences/2006-berlin/'>Berlin</a>
                 </li>
                 <li class='cityName conferencesAfrica'>
-                  <a href='#'>Johannesburg</a>
+                  <a href='/ua/conferences/2006-johannesburg/'>Johannesburg</a>
                 </li>
                 <li class='cityName conferencesSouthAmerica'>
-                  <a href='#'>Mexico City</a>
+                  <a href='/ua/conferences/2006-mexico-city/'>Mexico City</a>
                 </li>
                 <li class='cityName conferencesEurope'>
-                  <a href='#'>London</a>
+                  <a href='/ua/conferences/2005-london/'>London</a>
                 </li>
                 <li class='cityName conferencesAsia'>
-                  <a href='#'>Shanghai</a>
+                  <a href='/ua/conferences/2005-shanghai/'>Shanghai</a>
                 </li>
                 <li class='cityName conferencesNorthAmerica'>
-                  <a href='#'>New York</a>
+                  <a href='/ua/conferences/2005-new-york/'>New York</a>
                 </li>
               </ul>
             </nav>
