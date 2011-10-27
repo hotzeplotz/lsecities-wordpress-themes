ea40584ead5912055694deab2e7cce9ee140521c
andrea rota <a.rota@lse.ac.uk>
2011-10-27 16:22:33 +0100

    added trace output

--- twentyeleven-child-lsecities/nav.php
+++ twentyeleven-child-lsecities/nav.php
@@ -1,6 +1,8 @@
-<?php $children = wp_list_pages('title_li=&child_of='.$post->ID.'&echo=0');
-  if ($children) : ?>
-  <ul>
-  <?php echo $children; ?>
-  </ul>
-  <?php endif; ?>
+<?php
+if($TRACE_TEMPLATE_NAV) { error_log($post->ID); }
+$children = wp_list_pages('title_li=&child_of='.$post->ID.'&echo=0');
+if ($children) : ?>
+<ul>
+<?php echo $children; ?>
+</ul>
+<?php endif; ?>
