d8b1db66185631ea5aa457d2296423d5b1a82653
andrea rota <a.rota@lse.ac.uk>
2011-10-30 20:59:36 +0000

    indentation; moved down h2

--- twentyeleven-child-lsecities/pods-conferences-frontpage.php
+++ twentyeleven-child-lsecities/pods-conferences-frontpage.php
@@ -29,49 +29,48 @@ $button_links = $pod->get_field('links');
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<h1 class="entry-title"><?php echo $pod->get_field('name'); ?></h1>
-    <?php
-      $tagline = $pod->get_field('tagline');
-      if($tagline) : ?>
-      <h2><?php echo $tagline; ?></h2>
-    <?php endif; ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-<div class='row' id='core'>
-          <article class='ninecol'>
-            <div class='row'>
-              <div class='slider spaceAfter eightcol' id='slider'>
-                <a href='http://urban-age.net/publications/living-in-the-endless-city/' title='item'>
-                  <img alt='Living in the endless city - book cover' src='http://urban-age.net/media/homepage/sliders/201106_living-in-the-endless-city-book.jpg' title='New Publication: Living in the Endless City'>
-                </a>
-              </div>
-              <aside class='extras fourcol last'>
-              <?php echo do_shortcode($pod->get_field('info')); ?>
-              </aside>
-            </div>
-            <div class='introblurb'>
-              <?php echo do_shortcode($pod->get_field('abstract')); ?>
-            </div>
-            <div class='featureboxes clearfix row'>
-              <?php foreach($button_links as $key => $link) : ?>
-              <?php error_log('link key: ' . $key); ?>
-              <div class='featurebox fourcol<?php if((($key + 1) % 3) == 0) : ?> last<?php endif ; ?>'>
-                <a href="<?php echo $link['guid'] ; ?>" title="<?php echo $link['post_title'] ; ?>">
-                  <h3><?php echo $link['post_title'] ; ?></h3>
-                </a>
-              </div>
-              <?php endforeach ; ?>
-            </div>
-            <div id="the_content">
-            <?php the_content(); ?>
-            </div>
-          </article>
-          <aside class='threecol last'>
-            <?php get_template_part( 'nav', 'conferences' ); ?>
+    <div class='row' id='core'>
+      <article class='ninecol'>
+        <div class='row'>
+          <div class='slider spaceAfter eightcol' id='slider'>
+            <a href='http://urban-age.net/publications/living-in-the-endless-city/' title='item'>
+              <img alt='Living in the endless city - book cover' src='http://urban-age.net/media/homepage/sliders/201106_living-in-the-endless-city-book.jpg' title='New Publication: Living in the Endless City'>
+            </a>
+          </div>
+          <aside class='extras fourcol last'>
+          <?php echo do_shortcode($pod->get_field('info')); ?>
           </aside>
         </div>
-
-        
+        <?php
+          $tagline = $pod->get_field('tagline');
+          if($tagline) : ?>
+            <h2><?php echo $tagline; ?></h2>
+        <?php endif; ?>
+        <div class='introblurb'>
+          <?php echo do_shortcode($pod->get_field('abstract')); ?>
+        </div>
+        <div class='featureboxes clearfix row'>
+          <?php foreach($button_links as $key => $link) : ?>
+          <?php error_log('link key: ' . $key); ?>
+          <div class='featurebox fourcol<?php if((($key + 1) % 3) == 0) : ?> last<?php endif ; ?>'>
+            <a href="<?php echo $link['guid'] ; ?>" title="<?php echo $link['post_title'] ; ?>">
+              <h3><?php echo $link['post_title'] ; ?></h3>
+            </a>
+          </div>
+          <?php endforeach ; ?>
+        </div>
+        <div id="the_content">
+        <?php the_content(); ?>
+        </div>
+      </article>
+      <aside class='threecol last'>
+        <?php get_template_part( 'nav', 'conferences' ); ?>
+      </aside>
+    </div>
+    
 		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
 	</div><!-- .entry-content -->
 	<div class="entry-meta">
