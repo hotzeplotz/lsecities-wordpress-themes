d4f296e239c25132a25bb70d2411916751be6c76
andrea rota <a.rota@lse.ac.uk>
2011-10-30 07:45:00 +0000

    incorporated wide layout (activate via custom field)

--- twentyeleven-child-lsecities/content-page.php
+++ twentyeleven-child-lsecities/content-page.php
@@ -6,13 +6,16 @@
  * @subpackage Twenty_Eleven
  * @since Twenty Eleven 1.0
  */
+
+$wide_layout = strtolower(get_post_meta(get_the_ID(), "layout", true)) == 'wide' ? 1 : 0;
+$main_area_class = $wide_layout ? 'ninecol' : 'sixcol';
 ?>
 
 <div class="row">
 	<header class="entry-header">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 	</header><!-- .entry-header -->
-<article id="post-<?php the_ID(); ?>" <?php post_class('sixcol'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class($main_area_class); ?>>
 	<div class="entry-content">
 		<?php the_content(); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
@@ -21,11 +24,13 @@
 		<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
 </article><!-- #post-<?php the_ID(); ?> -->
+<?php if($wide_layout == false) : ?>
 <aside class="threecol">
   <?php
     echo get_post_meta(get_the_ID(), "aside", true);
   ?>
 </aside>
+<?php endif; ?>
 
 <?php get_template_part( 'nav'); ?>
 
