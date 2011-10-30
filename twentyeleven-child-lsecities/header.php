094ac9ad7deb36b33a55d8a46ecdc3632679704f
andrea rota <a.rota@lse.ac.uk>
2011-10-30 10:25:01 +0000

    removed extra closing tag

--- twentyeleven-child-lsecities/header.php
+++ twentyeleven-child-lsecities/header.php
@@ -160,7 +160,6 @@ $logo_element_id = $urban_age_section ? 'ualogo' : 'logo';
 				<?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu. The menu assiged to the primary position is the one used. If none is assigned, the menu with the lowest ID is used. */ ?>
 				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 			</nav> --> <!-- #access -->
-		</div><!-- .row -->
 	</header><!-- #branding -->
 
 	<div id="main" class="row">
