852953b82d19e2a871fe38b373d161c09a846367
andrea rota <a.rota@lse.ac.uk>
2011-10-31 15:18:36 +0000

    reverted previous change; left code (commented out)

--- twentyeleven-child-lsecities/nav-conferences.php
+++ twentyeleven-child-lsecities/nav-conferences.php
@@ -1,7 +1,44 @@
             <nav id='conferencesmenu'>
               <h2>Urban Age conferences</h2>
               <p>The Urban Age Programme is an international investigation of the spatial and social dynamics of cities centred on an annual conference, research initiative and publication.</p>
+              <!--
               <ul>
-                <?php wp_list_pages('title_li=&depth=1&child_of=96&sort_column=menu_order&sort_order=DESC&echo=1'); ?>
+                <?php // wp_list_pages('title_li=&depth=1&child_of=96&sort_column=menu_order&sort_order=DESC&echo=1'); ?>
+              </ul>
+              -->
+              <ul>
+                <li class='cityName conferencesAsia'>
+                  <a href='/ua/conferences/2011-hongkong/'>Hong Kong</a>
+                </li>
+                <li class='cityName conferencesNorthAmerica'>
+                  <a href='/ua/conferences/2010-chicago/'>Chicago</a>
+                </li>
+                <li class='cityName conferencesEurope'>
+                  <a href='/ua/conferences/2009-istanbul/'>Istanbul</a>
+                </li>
+                <li class='cityName conferencesSouthAmerica'>
+                  <a href='/ua/conferences/2008-sao-paulo/'>São Paulo</a>
+                </li>
+                <li class='cityName conferencesAsia'>
+                  <a href='/ua/conferences/2007-mumbai/'>Mumbai</a>
+                </li>
+                <li class='cityName conferencesEurope'>
+                  <a href='/ua/conferences/2006-berlin/'>Berlin</a>
+                </li>
+                <li class='cityName conferencesAfrica'>
+                  <a href='/ua/conferences/2006-johannesburg/'>Johannesburg</a>
+                </li>
+                <li class='cityName conferencesSouthAmerica'>
+                  <a href='/ua/conferences/2006-mexico-city/'>Mexico City</a>
+                </li>
+                <li class='cityName conferencesEurope'>
+                  <a href='/ua/conferences/2005-london/'>London</a>
+                </li>
+                <li class='cityName conferencesAsia'>
+                  <a href='/ua/conferences/2005-shanghai/'>Shanghai</a>
+                </li>
+                <li class='cityName conferencesNorthAmerica'>
+                  <a href='/ua/conferences/2005-new-york/'>New York</a>
+                </li>
               </ul>
             </nav>
