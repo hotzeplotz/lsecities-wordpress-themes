1940cda1e7c12713d0421c648c1bb0954b079cc6
andrea rota <a.rota@lse.ac.uk>
2011-11-02 14:07:59 +0000

    fixed extra ul

--- twentyeleven-child-lsecities/snippet-organizers.php
+++ twentyeleven-child-lsecities/snippet-organizers.php
@@ -5,8 +5,6 @@
       <img alt="LSE" src="<?php bloginfo('stylesheet_directory') ?>/images/lse_logo_blk.gif" height="49" />
     </a>
   </li>
-</ul>
-<ul>
   <li>
     <a href="http://www.alfred-herrhausen-gesellschaft.de" target="_blank">
       <img alt="Alfred Herrhausen Gesellschaft" src="<?php bloginfo('stylesheet_directory') ?>/images/ahs_logo_blk.gif" height="30" />
