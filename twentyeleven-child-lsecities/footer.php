f06d0f5c7e514b1dc1ea4a5daa6a035fe9d09d6d
andrea rota <a.rota@lse.ac.uk>
2011-11-02 12:05:20 +0000

    added rows

--- twentyeleven-child-lsecities/footer.php
+++ twentyeleven-child-lsecities/footer.php
@@ -12,64 +12,70 @@
 
 	</div><!-- #main -->
 
-	<footer id="footer" class="row">
-<nav id="footerSitemap">
-<section class="fourcol">
-<h3>About</h3>
-<ul>
- <?php wp_list_pages('title_li=&depth=1&child_of=617&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
-</ul>
-</section>
-<section class="fourcol">
-<h3>Who's who</h3>
-<ul>
- <?php wp_list_pages('title_li=&depth=1&child_of=421&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
-</ul>
-</section>
-<section class="fourcol last">
-<h3>Research</h3>
-<ul>
- <?php wp_list_pages('title_li=&depth=2&child_of=306&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
-</ul>
-</section>
-<section class="fourcol">
-<h3>Publications</h3>
-<ul>
- <?php wp_list_pages('title_li=&depth=2&child_of=309&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
-</ul>
-</section>
-<section class="fourcol">
-<h3>Conferences</h3>
-<ul>
- <?php wp_list_pages('title_li=&depth=1&child_of=96&sort_column=menu_order&sort_order=DESC&echo=1'); ?>
-</ul>
-</section>
-<section class="fourcol last">
-<h3>Events</h3>
-<ul>
- <?php wp_list_pages('title_li=&depth=1&child_of=311&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
-</ul>
-</section>
-<section class="fourcol">
-<h3>Deutsche Bank Urban Age Award</h3>
-<ul>
- <?php wp_list_pages('title_li=&depth=1&child_of=489&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
-</ul>
-</section>
-<section class="fourcol">
-<h3></h3>
-<ul>
- <?php // wp_list_pages('title_li=&depth=1&child_of=489&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
-</ul>
-</section>
-<section class="fourcol last">
-<h3>Organizers</h3>
-<ul>
- <?php get_template_part('snippet-organizers'); ?>
-</ul>
-</section>
-</nav>
-</footer><!-- #footer -->
+	<footer id="footer">
+    <nav id="footerSitemap">
+      <div class="row">
+        <section class="fourcol">
+          <h3>About</h3>
+          <ul>
+            <?php wp_list_pages('title_li=&depth=1&child_of=617&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
+          </ul>
+        </section>
+        <section class="fourcol">
+          <h3>Who's who</h3>
+          <ul>
+            <?php wp_list_pages('title_li=&depth=1&child_of=421&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
+          </ul>
+        </section>
+        <section class="fourcol last">
+          <h3>Research</h3>
+          <ul>
+            <?php wp_list_pages('title_li=&depth=2&child_of=306&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
+          </ul>
+        </section>
+      </div>
+      <div class="row">
+        <section class="fourcol">
+          <h3>Publications</h3>
+          <ul>
+            <?php wp_list_pages('title_li=&depth=2&child_of=309&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
+          </ul>
+        </section>
+        <section class="fourcol">
+          <h3>Conferences</h3>
+          <ul>
+            <?php wp_list_pages('title_li=&depth=1&child_of=96&sort_column=menu_order&sort_order=DESC&echo=1'); ?>
+          </ul>
+        </section>
+        <section class="fourcol last">
+          <h3>Events</h3>
+          <ul>
+            <?php wp_list_pages('title_li=&depth=1&child_of=311&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
+          </ul>
+        </section>
+      </div>
+      <div class="row">
+        <section class="fourcol">
+          <h3>Deutsche Bank Urban Age Award</h3>
+          <ul>
+            <?php wp_list_pages('title_li=&depth=1&child_of=489&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
+          </ul>
+        </section>
+        <section class="fourcol">
+          <h3></h3>
+          <ul>
+            <?php // wp_list_pages('title_li=&depth=1&child_of=489&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
+          </ul>
+        </section>
+        <section class="fourcol last">
+          <h3>Organizers</h3>
+          <ul>
+            <?php get_template_part('snippet-organizers'); ?>
+          </ul>
+        </section>
+      </div>
+    </nav>
+  </footer><!-- #footer -->
 </div><!-- #page -->
 </div><!-- ## grid # container -->
 
