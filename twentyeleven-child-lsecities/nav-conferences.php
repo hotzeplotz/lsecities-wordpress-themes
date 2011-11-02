834538c582d182f2043833936407270b2def6ab8
andrea rota <a.rota@lse.ac.uk>
2011-11-02 11:28:28 +0000

    generating city list via WP

--- twentyeleven-child-lsecities/nav-conferences.php
+++ twentyeleven-child-lsecities/nav-conferences.php
@@ -19,7 +19,7 @@
               <p>The Urban Age Programme is an international investigation of the spatial and social dynamics of cities centred on an annual conference, research initiative and publication.</p>
 
               <ul>
-                <?php // wp_list_pages('title_li=&depth=1&child_of=96&sort_column=menu_order&sort_order=DESC&echo=1'); ?>
+                <?php wp_list_pages('title_li=&depth=1&child_of=96&sort_column=menu_order&sort_order=DESC&echo=1'); ?>
               </ul>
               <!--
               <ul>
