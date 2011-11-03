5d152dc022d22c378f8fc2e7491a2fc4073acfac
andrea rota <a.rota@lse.ac.uk>
2011-11-03 10:51:18 +0000

    added Publications section navbar

--- twentyeleven-child-lsecities/nav.php
+++ twentyeleven-child-lsecities/nav.php
@@ -25,6 +25,8 @@ if($current_post_id == 94) : // /ua/ (Urban Age frontpage)
   get_template_part('snippet-organizers');
 elseif($current_post_id == 393) : // / (main frontpage)
   get_template_part('snippet-lsecities-frontpage');
+elseif($current_post_id == 309 or in_array(309, get_post_ancestors(309)) : // /publications (the whole Publications section)
+  get_template_part('nav/nav-publications.php');
 else :
   if($current_post_id) {
     $children = wp_list_pages('title_li=&depth=1&child_of='.$parent_post_id.'&echo=0');
---
+++ twentyeleven-child-lsecities/nav/nav-publications.php
@@ -0,0 +1,3 @@
+<ul>
+  <?php echo wp_list_pages('title_li=&depth=0&child_of=309&echo=0'); ?>
+</ul>
