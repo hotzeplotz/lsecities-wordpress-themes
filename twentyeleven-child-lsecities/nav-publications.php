3ef19cbcab85e17e6f76447e6e89263888a17868
andrea rota <a.rota@lse.ac.uk>
2011-11-04 09:34:01 +0000

    sorting of navmenu items

--- twentyeleven-child-lsecities/nav-publications.php
+++ twentyeleven-child-lsecities/nav-publications.php
@@ -1,5 +1,5 @@
 <?php $PUBLICATIONS_TOP_PAGE = 309; ?>
 <h3><?php echo get_the_title($PUBLICATIONS_TOP_PAGE); ?></h3>
 <ul>
-  <?php echo wp_list_pages("title_li=&depth=0&child_of=$PUBLICATIONS_TOP_PAGE&echo=0"); ?>
+  <?php echo wp_list_pages("title_li=&depth=0&child_of=$PUBLICATIONS_TOP_PAGE&echo=0&sort_column=menu_order&sort_order=ASC"); ?>
 </ul>
