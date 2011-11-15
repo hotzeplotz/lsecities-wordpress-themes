dc207a1abe47ad0b1456dc8f46d8d1cc98789f0c
andrea rota <a.rota@lse.ac.uk>
2011-11-15 15:39:00 +0000

    removed annoying entry-meta footer

--- twentyeleven-child-lsecities/content-page.php
+++ twentyeleven-child-lsecities/content-page.php
@@ -20,9 +20,6 @@ $main_area_class = $wide_layout ? 'ninecol' : 'sixcol';
 		<?php the_content(); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
 	</div><!-- .entry-content -->
-	<footer class="entry-meta">
-		<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-meta -->
 </article><!-- #post-<?php the_ID(); ?> -->
 <?php if($wide_layout == false) : ?>
 <aside class="threecol">
