26257aa6093be640a5dbbed7b0c6a10c55e9b29a
andrea rota <a.rota@lse.ac.uk>
2011-11-16 22:53:40 +0000

    removed Perlism

--- twentyeleven-child-lsecities-urbanage/pods-publication.php
+++ twentyeleven-child-lsecities-urbanage/pods-publication.php
@@ -87,7 +87,7 @@ $PODS_BASEURI_ARTICLES = 'http://urban-age.net/media/objects/articles';
         if(preg_match("/^" . $section['id'] . "/", $articles_pods->get_field('sequence'))) :
           $article_authors = $articles_pods->get_field('authors');
           foreach($article_authors as $author) {
-            $author_names =. $author['name'] . ' ' . $author['family_name'] . ', ';
+            $author_names = $author_names . $author['name'] . ' ' . $author['family_name'] . ', ';
           }
           $article_name = $articles_pods->get_field('name');
           echo '<!--' . $author_names . ' ' . $article_name '-->';
@@ -95,7 +95,7 @@ $PODS_BASEURI_ARTICLES = 'http://urban-age.net/media/objects/articles';
           <!-- <?php echo 'article Pod object: ' . var_export($articles_pods, true); ?> -->
           <?php endif; ?>
           <li>
-            <?php echo '<!--' . $author_names . ' ' ; ?><a href="<?php echo $PODS_BASEURI_ARTICLES . '/' . $articles_pods->get_field('slug'); ?>"><?php echo $articles_title; ?></a>
+            <?php echo '<!--' . $author_names ; ?><a href="<?php echo $PODS_BASEURI_ARTICLES . '/' . $articles_pods->get_field('slug'); ?>"><?php echo $articles_title; ?></a>
             <?php if(!empty($article['language']['name'])) : ?>
               (English) - <a href="<?php echo $PODS_BASEURI_ARTICLES . '/' . $article['slug'] . '/?lang=' . $article['language']['language_code']; ?>">(<?php echo $article['language']['name']; ?>)</a>
             <?php endif; ?>
