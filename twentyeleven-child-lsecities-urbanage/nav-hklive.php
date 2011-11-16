63d8a07a784035c77dd18ae2e58812386d07091f
andrea rota <a.rota@lse.ac.uk>
2011-11-16 13:27:35 +0000

    added webstreaming partner note

--- twentyeleven-child-lsecities-urbanage/nav-hklive.php
+++ twentyeleven-child-lsecities-urbanage/nav-hklive.php
@@ -1,9 +1,9 @@
 <?php
 // ad-hoc template for the Hong Kong conference live streaming page
 ?>
-<p>The Urban Age Hong Kong conference is broadcast live here on <strong>Wednesday 16 November</strong> between <strong>8:30AM and 6PM Hong Kong time</strong>
+<p>The Urban Age Hong Kong conference is broadcast live here on <strong>Thursday 17 November</strong> between <strong>8:30AM and 5:30PM Hong Kong time</strong>
 </p>
-<p>UK: 0:30AM to 10AM</p>
-<p>EST: 7:30PM (15 November) to 5AM</p>
-<p>PST: 4:30PM (15 November) to 2AM</p>
-<p>The live stream is available at the same times on <strong>Thursday 17 November</strong>.</p>
+<p>UK: 0:30AM to 9:30AM</p>
+<p>EST: 7:30PM (15 November) to 4:30AM</p>
+<p>PST: 4:30PM (15 November) to 1:30AM</p>
+<p><em>Webcast partner: <a href="http://smart-streaming.com/">smart-streaming.com</a></em></p>
