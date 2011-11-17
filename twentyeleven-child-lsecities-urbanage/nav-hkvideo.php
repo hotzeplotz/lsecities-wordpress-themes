5bb1139484680c1b5e0fc42a8968605146f79cd9
andrea rota <a.rota@lse.ac.uk>
2011-11-17 16:44:28 +0000

    added para

--- twentyeleven-child-lsecities-urbanage/nav-hkvideo.php
+++ twentyeleven-child-lsecities-urbanage/nav-hkvideo.php
@@ -4,13 +4,15 @@
 <p>The Urban Age Hong Kong live streaming has now ended.</p>
 <p>Videos of the sessions of the first day of the conference can be watched on this page. Videos of the second day will be posted soon.</p>
 <h3>16 November</h3>
-<ul>
-  <li><a href="#session001">Welcome</a></li>
-  <li><a href="#session002">The politics of urban health</a></li>
-  <li><a href="#session003">Understanding health in cities</a></li>
-  <li><a href="#session004">Measuring quality of life</a></li>
-  <li><a href="#session005">Space and design</a></li>
-  <li><a href="#session006">Designing for density</a></li>
-  <li>Evening keynote: <a href="#session007">Beyond inequality: emerging logics of expulsion</a></li>
-</ul>
+<p>
+  <ul>
+    <li><a href="#session001">Welcome</a></li>
+    <li><a href="#session002">The politics of urban health</a></li>
+    <li><a href="#session003">Understanding health in cities</a></li>
+    <li><a href="#session004">Measuring quality of life</a></li>
+    <li><a href="#session005">Space and design</a></li>
+    <li><a href="#session006">Designing for density</a></li>
+    <li>Evening keynote: <a href="#session007">Beyond inequality: emerging logics of expulsion</a></li>
+  </ul>
+</p>
 <p><em>Webcast partner: <a href="http://smart-streaming.com/">Smart Streaming Limited</a></em></p>
