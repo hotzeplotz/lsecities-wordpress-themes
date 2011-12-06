9829b0db6734bec8977bd6e2f50b8c1079d5e70c
andrea rota <a.rota@lse.ac.uk>
2011-12-06 16:57:02 +0000

    fixed menu

--- twentyeleven-child-lsecities/nav.php
+++ twentyeleven-child-lsecities/nav.php
@@ -31,7 +31,7 @@ elseif($current_post_id == 309 or in_array(309, get_post_ancestors($current_post
   get_template_part('nav-publications');
 elseif($current_post_id == 595) : // /ua/conferences/2011-hongkong/audio-and-video/
   get_template_part('nav-hkvideo');
-elseif($current_post_id == 577 or in_array(577, get_post_ancestors($current_post_id)) : // /ua/conferences/2011-hongkong
+elseif($current_post_id == 577 or in_array(577, get_post_ancestors($current_post_id))) : // /ua/conferences/2011-hongkong
   get_template_part('nav-hk2011');
 else :
   if($current_post_id) {
