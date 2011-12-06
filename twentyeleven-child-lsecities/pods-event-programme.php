73d02ef8ea9728a69466aa0fc638183ac9604d1d
andrea rota <a.rota@lse.ac.uk>
2011-12-06 16:41:27 +0000

    added tracing

--- twentyeleven-child-lsecities/pods-event-programme.php
+++ twentyeleven-child-lsecities/pods-event-programme.php
@@ -57,6 +57,7 @@ function process_session($session_slug) {
   $session_youtube_video = $pod->get_field('media_items.youtube_uri');
   $subsessions = $pod->get_field('sessions.slug');
   // if(count($subsessions) == 1) { $subsessions = array(0 => $subsessions); }
+  if($TRACE_PODS_EVENT_PROGRAMME) { error_log($TRACE_PREFIX . 'session count: ' . count($subsessions)); }
   if($TRACE_PODS_EVENT_PROGRAMME) { error_log($TRACE_PREFIX . 'sessions: ' . var_export($subsessions, true)); }
   if($session_title and !$hide_title) { echo '<h1>' . $session_times . $session_title . '</h1>'; }
   if($session_subtitle and !$hide_title) { echo "<h2>$session_subtitle</h2>"; }
