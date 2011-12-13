a6a458d20b1ba1ef7ef529228e1172ab904cf3d2
andrea rota <a.rota@lse.ac.uk>
2011-12-13 12:44:52 +0000

    added tracing

--- twentyeleven-child-lsecities/pods-events-frontpage.php
+++ twentyeleven-child-lsecities/pods-events-frontpage.php
@@ -30,6 +30,9 @@ if($pod_slug) {
 if($TRACE_PODS_EVENTS_FRONTPAGE) { error_log('pod_slug: ' . $pod_slug); }
 
 $button_links = $pod->get_field('links');
+
+if($TRACE_PODS_EVENTS_FRONTPAGE) { error_log(var_export($button_links, true)); }
+
 $slider = $pod->get_field('slider');
 if(!$slider) {
   $featured_image_uri = get_the_post_thumbnail(get_the_ID(), array(960,367));
