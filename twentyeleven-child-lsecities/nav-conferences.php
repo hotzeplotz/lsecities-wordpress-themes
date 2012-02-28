4c84174a6c4b28816d5cb7e5beb275ac0d2820a3
andrea rota <a.rota@lse.ac.uk>
2012-02-28 14:52:31 +0000

    removed list of conferences

--- twentyeleven-child-lsecities/nav-conferences.php
+++ twentyeleven-child-lsecities/nav-conferences.php
@@ -24,9 +24,11 @@ else :
               <h2>Urban Age conferences</h2>
               <p>The Urban Age Programme is an international investigation of the spatial and social dynamics of cities centred on an annual conference, research initiative and publication.</p>
 
+<!--
               <ul>
                 <?php wp_list_pages('title_li=&depth=1&child_of=96&sort_column=menu_order&sort_order=DESC&echo=1'); ?>
               </ul>
+-->
               <!--
               <ul>
                 <li class='cityName conferencesAsia'>
