cdfa4129f30deb48ef801279b7ab250ba126fe28
andrea rota <a.rota@lse.ac.uk>
2012-03-19 10:31:30 +0000

    updated eval

--- twentyeleven-child-lsecities/pods-main-frontpage.php
+++ twentyeleven-child-lsecities/pods-main-frontpage.php
@@ -72,7 +72,7 @@ $slides = $pod->get_field('slides');
                         if($TRACE_PODS_MAIN_FRONTPAGE) { echo '<!-- tile[layout]: ' . var_export($tile_layout, true) . ' -->'; }
                         $this_tile_count = preg_replace('/x/', '*', $tile_layout[0]['name']);
                         if($TRACE_PODS_MAIN_FRONTPAGE) { echo '<!-- this_tile_count: ' . var_export($this_tile_count, true) . ' -->'; }
-                        $this_tile_count = eval('$this_tile_count = ' . $this_tile_count . ';');
+                        eval('$this_tile_count = ' . $this_tile_count . ';');
                         $tile_count -= $this_tile_count;
                         if($TRACE_PODS_MAIN_FRONTPAGE) { echo '<!-- tile_count: ' . var_export($tile_count, true) . ' -->'; }
                         array_push($slide_column, array(                            
