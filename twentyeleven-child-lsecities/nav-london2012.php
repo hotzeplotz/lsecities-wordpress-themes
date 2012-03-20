978d3b40b039de6500f5570510a54690a2e7ddf3
andrea rota <a.rota@lse.ac.uk>
2012-03-20 10:15:22 +0000

    fixed link

--- twentyeleven-child-lsecities/nav-london2012.php
+++ twentyeleven-child-lsecities/nav-london2012.php
@@ -12,7 +12,7 @@ $IMG_PREFIX = $BASE_URI . '/ua/conferences/2012-london/';
   <div class="defaultBox">
    <h1>Newsletter subscription</h1>
    <p>
-    If you would like to receive updates on LSE Cities and Urban Age activities and research – including notices related to the <em>Electric City conference</em> – please <a href="publications/ebulletins/subscription/">click here</a>.
+    If you would like to receive updates on LSE Cities and Urban Age activities and research – including notices related to the <em>Electric City conference</em> – please <a href="/publications/ebulletins/subscription/">click here</a>.
    </p>
   </div>
  
