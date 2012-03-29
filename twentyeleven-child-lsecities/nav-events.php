d67c455b7fe4d8e4f70f78bd0aba5db8c22f0f17
andrea rota <a.rota@lse.ac.uk>
2012-03-29 11:27:12 +0100

    display future events

--- twentyeleven-child-lsecities/nav-events.php
+++ twentyeleven-child-lsecities/nav-events.php
@@ -10,10 +10,27 @@ pod:
 -->
 <?php endif; 
 
-// prepare array with list of events, split by year
+$events_pod = new Pod('event');
+$datetime_now = new DateTime('now');
+if($TRACE_TEMPLATE_NAV) { error_log($TRACE_PREFIX . 'datetime_now: ' . $datetime_now); 
+
+// prepare array with list of upcoming events
+$upcoming_events = Array();
+$events_pod->findRecords(array(
+  'where' => "t.date_start < $datetime_now",
+  'orderby' => "t.date_start ASC"
+));
+while($events_pod->fetchRecord()) {
+  array_push($upcoming_events, array(
+    'slug' => $events_pod->get_field('slug'),
+    'name' => $events_pod->get_field('name'),
+    'date' => date('d F', strtotime($events_pod->get_field('date_start')))
+  ));
+}
+
+// prepare array with list of past events, split by year
 $events = Array();
 $current_year = date("Y");
-$events_pod = new Pod('event');
 
 for($year = 2005; $year <= $current_year; $year++) {
   $events[$year] = Array();
@@ -24,11 +41,14 @@ for($year = 2005; $year <= $current_year; $year++) {
   ));
   if($TRACE_TEMPLATE_NAV) { error_log($TRACE_PREFIX . 'events records found: ' . $events_pod->getTotalRows()); }
   while($events_pod->fetchRecord()) {
-    array_push($events[$year], Array(
-      'slug' => $events_pod->get_field('slug'),
-      'name' => $events_pod->get_field('name'),
-      'date' => date('d F', strtotime($events_pod->get_field('date_start')))
-    ));
+    // if event is past, add it to array
+    if($events_pod->get_field['date_start'] < $datetime_now) {
+      array_push($events[$year], Array(
+	'slug' => $events_pod->get_field('slug'),
+	'name' => $events_pod->get_field('name'),
+	'date' => date('d F', strtotime($events_pod->get_field('date_start')))
+      ));
+    }
   }
   
   // if there are no events for this year, remove year's array altogether from full list
@@ -39,22 +59,40 @@ for($year = 2005; $year <= $current_year; $year++) {
 
 if($TRACE_TEMPLATE_NAV) { echo "<!-- $TRACE_PREFIX events array: \n" . var_export($events, true) . " \n-->"; }
 
+// sort by year, backwards from current year
 krsort($events);
 ?>
 
 <nav id="eventsmenu">
-<?php if($events): ?>
   <dl>
-  <?php foreach($events as $year => $year_events): ?>
-    <dt><?php echo $year ?></dt>
+    <dt class="events upcoming">Upcoming events</dt>
     <dd>
+    <?php if($upcoming_events): ?>
       <ul>
-      <?php foreach($year_events as $event): ?>
+      <?php foreach($upcoming_events as $event): ?>
 	<li><a href="<?php echo $BASE_URI . $event['slug'] ?>"><?php echo $event['date']; ?> | <?php echo $event['name']; ?></a></li>
       <?php endforeach; ?>
       </ul>
+    <?php else: ?>
+      <p>Please check back for more information on our upcoming events.</p>
+    <?php endif; ?>
     </dd>
-  <?php endforeach; ?>
-  </ul>
-<?php endif; ?>
+    <?php if($events): ?>
+    <dt class="events past">Past events</dt>
+    <dd>
+      <dl>
+      <?php foreach($events as $year => $year_events): ?>
+	<dt><?php echo $year ?></dt>
+	<dd>
+	  <ul>
+	  <?php foreach($year_events as $event): ?>
+	    <li><a href="<?php echo $BASE_URI . $event['slug'] ?>"><?php echo $event['date']; ?> | <?php echo $event['name']; ?></a></li>
+	  <?php endforeach; ?>
+	  </ul>
+	</dd>
+      <?php endforeach; ?>
+      </dl>
+    </dd>
+    <?php endif; ?>
+  </dl>
 </nav>
