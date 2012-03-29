281f2fd3afd243ea1b7668dbfc61fdb3ba5b044f
andrea rota <a.rota@lse.ac.uk>
2012-03-29 10:03:45 +0100

    updated tracing

--- twentyeleven-child-lsecities/nav-events.php
+++ twentyeleven-child-lsecities/nav-events.php
@@ -30,7 +30,7 @@ for($year = 2005; $year <= $current_year; $year++) {
       'date' => date('d F', strtotime($events_pod->get_field('date_start')))
     ));
   }
-  if($TRACE_TEMPLATE_NAV) { error_log($TRACE_PREFIX . 'events array: ' . $events); }
+  if($TRACE_TEMPLATE_NAV) { error_log($TRACE_PREFIX . 'events array: ' . var_export($events, true)); }
 }
 
 ?>
