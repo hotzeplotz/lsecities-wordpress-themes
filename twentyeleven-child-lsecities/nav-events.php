d82ad33e60248afb6e46537cf775d72cdbc504c0
andrea rota <a.rota@lse.ac.uk>
2012-03-30 17:15:32 +0100

    pure-sql way

--- twentyeleven-child-lsecities/nav-events.php
+++ twentyeleven-child-lsecities/nav-events.php
@@ -17,8 +17,9 @@ if($TRACE_TEMPLATE_NAV) { error_log($TRACE_PREFIX . 'datetime_now: ' . $datetime
 // prepare array with list of upcoming events
 $upcoming_events = Array();
 $events_pod->findRecords(array(
-  'where' => 't.date_start > ' . $datetime_now->format('Y-m-d'),
-  'orderby' => 't.date_start ASC'
+  'where' => 't.date_start > NOW()',
+  'orderby' => 't.date_start ASC',
+  'limit' => 5
 ));
 while($events_pod->fetchRecord()) {
   array_push($upcoming_events, array(
