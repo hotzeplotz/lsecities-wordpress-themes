1eefc0bd47d0bb47dc8582d30239ec78505d9014
andrea rota <a.rota@lse.ac.uk>
2012-04-03 16:25:13 +0100

    do not output header stuff if no heading photo is given

--- twentyeleven-child-lsecities/pods-events-frontpage.php
+++ twentyeleven-child-lsecities/pods-events-frontpage.php
@@ -122,13 +122,13 @@ $poster_pdf = $poster_pdf[0]['guid'];
 
           <div class='ninecol' id='contentarea'>
             <div class='top-content'>
+              <?php if($featured_image_uri) : ?>
               <header class='heading-image'>
                 <div class='photospread wireframe'>
-                <?php if($featured_image_uri) : ?>
                   <img src="<?php echo $featured_image_uri; ?>" alt="event photo" />
-                <?php endif; ?>
                 </div>
               </header>
+              <?php endif; ?>
               <article class='wireframe eightcol'>
                 <header>
                   <h1><?php echo $pod->get_field('name'); ?></h1>
