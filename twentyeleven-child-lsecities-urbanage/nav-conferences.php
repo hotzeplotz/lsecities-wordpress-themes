c4cf170755f45d80361cfd4eedbcb006315eabe6
andrea rota <a.rota@lse.ac.uk>
2012-04-19 11:11:48 +0100

    removed obsolete code

--- twentyeleven-child-lsecities-urbanage/nav-conferences.php
+++ twentyeleven-child-lsecities-urbanage/nav-conferences.php
@@ -6,7 +6,7 @@ elseif($current_post_id == 1388 or in_array(1388, get_post_ancestors($current_po
   get_template_part('nav', 'london2012');
 else :
 ?>
-            <nav id='conferencesmenu'>
+<nav id='conferencesmenu'>
 <?php if($post->ID == 94) : // /ua/ ?>
   <div>
     <h1>Organisers</h1>
@@ -23,48 +23,5 @@ else :
     </span>
   </div>   
 <?php endif; ?>
-              <h2>Urban Age conferences</h2>
-              <p>The Urban Age Programme is an international investigation of the spatial and social dynamics of cities centred on an annual conference, research initiative and publication.</p>
-              <ul>
-                <?php wp_list_pages('title_li=&depth=1&child_of=96&sort_column=menu_order&sort_order=DESC&echo=1'); ?>
-              </ul>
-
-              <!--
-              <ul>
-                <li class='cityName conferencesAsia'>
-                  <a href='/ua/conferences/2011-hongkong/'>2011 | Hong Kong</a>
-                </li>
-                <li class='cityName conferencesNorthAmerica'>
-                  <a href='/ua/conferences/2010-chicago/'>2010 | Chicago</a>
-                </li>
-                <li class='cityName conferencesEurope'>
-                  <a href='/ua/conferences/2009-istanbul/'>2009 | Istanbul</a>
-                </li>
-                <li class='cityName conferencesSouthAmerica'>
-                  <a href='/ua/conferences/2008-sao-paulo/'>2008 | São Paulo</a>
-                </li>
-                <li class='cityName conferencesAsia'>
-                  <a href='/ua/conferences/2007-mumbai/'>2007 | Mumbai</a>
-                </li>
-                <li class='cityName conferencesEurope'>
-                  <a href='/ua/conferences/2006-berlin/'>2006 | Berlin</a>
-                </li>
-                <li class='cityName conferencesAfrica'>
-                  <a href='/ua/conferences/2006-johannesburg/'>2006 | Johannesburg</a>
-                </li>
-                <li class='cityName conferencesSouthAmerica'>
-                  <a href='/ua/conferences/2006-mexico-city/'>2006 | Mexico City</a>
-                </li>
-                <li class='cityName conferencesEurope'>
-                  <a href='/ua/conferences/2005-london/'>2005 | London</a>
-                </li>
-                <li class='cityName conferencesAsia'>
-                  <a href='/ua/conferences/2005-shanghai/'>2005 | Shanghai</a>
-                </li>
-                <li class='cityName conferencesNorthAmerica'>
-                  <a href='/ua/conferences/2005-new-york/'>2005 | New York</a>
-                </li>
-              </ul>
--->
-            </nav>
+</nav>
 <?php endif; ?>
