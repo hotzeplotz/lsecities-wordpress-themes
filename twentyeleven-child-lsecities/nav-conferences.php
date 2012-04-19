fc34451f79b637e2a913985e497fa5863a35b0a3
andrea rota <a.rota@lse.ac.uk>
2012-04-19 09:47:25 +0100

    fixed syntax

--- twentyeleven-child-lsecities/nav-conferences.php
+++ twentyeleven-child-lsecities/nav-conferences.php
@@ -6,7 +6,7 @@ function check_parent_conference($post_id) {
   $current_post_id = $post->ID;
   var_trace('current_post_id: ' . $current_post_id, $TRACE_PREFIX, $TRACE_ENABLED);
   if($current_post_id == $post_id or in_array($post_id, get_post_ancestors($current_post_id))) {
-    return true
+    return true;
   } else {
     return false;
   }
