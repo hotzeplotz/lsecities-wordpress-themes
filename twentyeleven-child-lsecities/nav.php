224bafa4f2a4c1703d246221eeea325e6a199dfc
andrea rota <a.rota@lse.ac.uk>
2012-04-19 09:06:41 +0100

    backporting new structure to older templates

--- twentyeleven-child-lsecities/nav.php
+++ twentyeleven-child-lsecities/nav.php
@@ -38,10 +38,14 @@ else :
     $children = wp_list_pages('title_li=&depth=1&child_of='.$parent_post_id.'&echo=0');
   }
   if ($children) : ?>
-  <h3><?php echo get_the_title($parent_post_id); ?></h3>
-  <ul>
-    <?php echo $children; ?>
-  </ul>
+  <dl>
+    <dt><?php echo get_the_title($parent_post_id); ?></dt>
+    <dd>
+      <ul>
+      <?php echo $children; ?>
+      </ul>
+    </dd>
+  </dl>
   <?php else : ?>
   &#160;
   <?php endif;
