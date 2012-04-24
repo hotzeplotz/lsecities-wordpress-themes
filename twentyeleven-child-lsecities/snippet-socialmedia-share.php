aee27a7c66fda653aba1682bb7d0bfe77c881f91
andrea rota <a.rota@lse.ac.uk>
2012-04-24 11:15:12 +0100

    updated path

--- twentyeleven-child-lsecities/snippet-socialmedia-share.php
+++ twentyeleven-child-lsecities/snippet-socialmedia-share.php
@@ -3,12 +3,12 @@
   <ul>
     <li>
       <a href="https://www.facebook.com/sharer.php?u=<?php ?>&t=<?php ?>">
-        <img alt="Share on Facebook" src="<?php bloginfo('stylesheet_directory') ?>icon_facebook-v2darkblue_24x24.png" />
+        <img alt="Share on Facebook" src="<?php bloginfo('stylesheet_directory') ?>/images/icons/mal/icon_facebook-v2darkblue_24x24.png" />
       </a>
     </li>
     <li>
       <a href="https://twitter.com/share?url=<?php ?>">
-        <img alt="Share on Twitter" src="<?php bloginfo('stylesheet_directory') ?>icon_twitter-v2darkblue_24x24.png" />
+        <img alt="Share on Twitter" src="<?php bloginfo('stylesheet_directory') ?>/images/icons/mal/icon_twitter-v2darkblue_24x24.png" />
       </a>
     </li>
   </ul>
