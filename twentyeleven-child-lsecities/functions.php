0a772cf2fdf09dcd19f8c16c2f3db36cbf193804
andrea rota <a.rota@lse.ac.uk>
2012-04-24 17:37:37 +0100

    passing URI and title to social media share pages

--- twentyeleven-child-lsecities/functions.php
+++ twentyeleven-child-lsecities/functions.php
@@ -43,3 +43,16 @@ function shorten_string($string, $wordsreturned) {
   }
   return $retval;
 }
+
+// from http://webcheatsheet.com/php/get_current_page_url.php
+function get_current_page_URI() {
+ $pageURL = 'http';
+ if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+ $pageURL .= "://";
+ if ($_SERVER["SERVER_PORT"] != "80") {
+  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+ } else {
+  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+ }
+ return $pageURL;
+}
--- twentyeleven-child-lsecities/snippet-socialmedia-share.php
+++ twentyeleven-child-lsecities/snippet-socialmedia-share.php
@@ -2,12 +2,12 @@
   <h1>Share</h1>
   <ul>
     <li>
-      <a href="https://www.facebook.com/sharer.php?u=<?php ?>&t=<?php ?>">
+      <a href="https://www.facebook.com/sharer.php?u=<?php echo get_current_page_URI(); ?>&t=<?php wp_title( '|', true, 'right' ); bloginfo( 'name' ); ?>">
         <img alt="Share on Facebook" src="<?php bloginfo('stylesheet_directory') ?>/images/icons/mal/icon_facebook-v2darkblue_24x24.png" />
       </a>
     </li>
     <li>
-      <a href="https://twitter.com/share?url=<?php ?>">
+      <a href="https://twitter.com/share?url=<?php echo get_current_page_URI(); ?>">
         <img alt="Share on Twitter" src="<?php bloginfo('stylesheet_directory') ?>/images/icons/mal/icon_twitter-v2darkblue_24x24.png" />
       </a>
     </li>
