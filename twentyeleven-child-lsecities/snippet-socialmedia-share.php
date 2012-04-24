d102245d27db544a02fcfed9da8889a70e2acc8c
andrea rota <a.rota@lse.ac.uk>
2012-04-24 11:13:34 +0100

    actually added snippet

---
+++ twentyeleven-child-lsecities/snippet-socialmedia-share.php
@@ -0,0 +1,15 @@
+<div id="share-on-social-media" class="clearfix">
+  <h1>Share</h1>
+  <ul>
+    <li>
+      <a href="https://www.facebook.com/sharer.php?u=<?php ?>&t=<?php ?>">
+        <img alt="Share on Facebook" src="<?php bloginfo('stylesheet_directory') ?>icon_facebook-v2darkblue_24x24.png" />
+      </a>
+    </li>
+    <li>
+      <a href="https://twitter.com/share?url=<?php ?>">
+        <img alt="Share on Twitter" src="<?php bloginfo('stylesheet_directory') ?>icon_twitter-v2darkblue_24x24.png" />
+      </a>
+    </li>
+  </ul>
+</div>
