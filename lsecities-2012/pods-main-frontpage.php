449aba2c0367a70dd3cce9bac0c9aa8dd55c371a
andrea rota <a.rota@lse.ac.uk>
2012-04-29 20:22:33 +0100

    do not display news if not requested to

--- lsecities-2012/pods-main-frontpage.php
+++ lsecities-2012/pods-main-frontpage.php
@@ -219,50 +219,49 @@ $slides = $pod->get_field('slides');
                 <?php endforeach; ?>
               </ul>
             </div>
-            
-            
-                <div class="extra-content">
-      <section id='news_area'>
-        <header>
-          <h1>News</h1>
-        </header>
-        <div class='clearfix row'>
-          <?php $latest_news = new WP_Query('posts_per_page=3' . $news_categories);
-            while ($latest_news->have_posts()) :
-              $latest_news->the_post();
-              $do_not_duplicate = $post->ID;
-              if($latest_news->current_post == 2) { $class_extra = " last"; }
-            ?>
-          <div class='fourcol<?php echo $class_extra; ?>'>
-            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-            <?php the_excerpt(); ?>
-          </div>
-          <?php endwhile;
-            wp_reset_postdata();
-          ?>
-        </div><!--.clearfix.row -->
-        <?php $more_news = new WP_Query('posts_per_page=10' . $news_categories);
-          if($more_news->found_posts > 3) :
-        ?>
-        <ul>
-        <?php
-            while ($more_news->have_posts()) :
-              $more_news->the_post();
-              if ($more_news->current_post > 2) :
-        ?>
-          <li><a href="<?php the_permalink(); ?>"><?php the_time('j M'); ?> | <?php the_title() ?></a></li>
-        <?php endif;
-            endwhile;
-        ?>
-        </ul>
-        <?php
-          endif;
-        ?>
-        </ul>
-      </section><!-- #news_area -->
-    </div><!-- .extra-content -->
 
-
+            <div class="extra-content">
+              <?php if($news_categories): ?>
+              <section id='news_area'>
+                <header>
+                  <h1>News</h1>
+                </header>
+                <div class='clearfix row'>
+                  <?php $latest_news = new WP_Query('posts_per_page=3' . $news_categories);
+                    while ($latest_news->have_posts()) :
+                      $latest_news->the_post();
+                      $do_not_duplicate = $post->ID;
+                      if($latest_news->current_post == 2) { $class_extra = " last"; }
+                    ?>
+                  <div class='fourcol<?php echo $class_extra; ?>'>
+                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                    <?php the_excerpt(); ?>
+                  </div>
+                  <?php endwhile;
+                    wp_reset_postdata();
+                  ?>
+                </div><!--.clearfix.row -->
+                <?php $more_news = new WP_Query('posts_per_page=10' . $news_categories);
+                  if($more_news->found_posts > 3) :
+                ?>
+                <ul>
+                <?php
+                    while ($more_news->have_posts()) :
+                      $more_news->the_post();
+                      if ($more_news->current_post > 2) :
+                ?>
+                  <li><a href="<?php the_permalink(); ?>"><?php the_time('j M'); ?> | <?php the_title() ?></a></li>
+                <?php endif;
+                    endwhile;
+                ?>
+                </ul>
+                <?php
+                  endif;
+                ?>
+                </ul>
+              </section><!-- #news_area -->
+              <?php endif; ?>
+            </div><!-- .extra-content -->
       </div>      
     </div><!-- #core.row -->
     </div>        
