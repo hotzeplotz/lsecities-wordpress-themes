e026a0767505ec778105a807e3c6225803536c27
andrea rota <a.rota@lse.ac.uk>
2012-05-02 14:24:31 +0100

    updated URI generation

--- lsecities-2012/snippet-organizers.php
+++ lsecities-2012/snippet-organizers.php
@@ -1,13 +1,14 @@
+<?php global $urban_age_section; ?>
 <ul id="organizer-logos">
   <li>
     <a href="http://www.lse.ac.uk/" target="_blank">
-      <img alt="LSE" src="<?php bloginfo('stylesheet_directory') ?>/images/lse_logo_white.gif" height="49" />
+      <img alt="LSE" src="<?php bloginfo('stylesheet_directory') ?>/images/lse_logo_white.gif" />
     </a>
   </li>
-  <?php if(false): // disabling AHS logo for the moment ?>
+  <?php if($urban_age_section): ?>
   <li>
-    <a href="http://www.alfred-herrhausen-gesellschaft.de" target="_blank">
-      <img alt="Alfred Herrhausen Gesellschaft" src="<?php bloginfo('stylesheet_directory') ?>/images/ahs_logo_blk.gif" height="30" />
+    <a href="http://www.alfred-herrhausen-gesellschaft.de/en/" target="_blank">
+      <img alt="Alfred Herrhausen Gesellschaft" src="<?php bloginfo('stylesheet_directory') ?>/images/ahs_logo_white.gif" />
     </a>
   </li>
   <?php endif; ?>
