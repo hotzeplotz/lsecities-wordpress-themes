192f9fcc28e7a46f51906f1be1505ab4da65cc91
andrea rota <a.rota@lse.ac.uk>
2012-05-02 18:23:23 +0100

    removed obsolete code

--- lsecities-2012/snippet-organizers.php
+++ lsecities-2012/snippet-organizers.php
@@ -1,15 +1,3 @@
-<?php 
-/*$TRACE_ENABLED = is_user_logged_in();
-$TRACE_PREFIX = 'footer';
-$ancestors = get_ancestors($post->ID, 'page');
-echo var_trace(var_export($ancestors, true), $TRACE_PREFIX, $TRACE_ENABLED);
-array_unshift($ancestors, $post->ID);
-echo var_trace(var_export($ancestors, true), $TRACE_PREFIX, $TRACE_ENABLED);
-$toplevel_ancestor = array_pop($ancestors);
-echo var_trace($toplevel_ancestor, $TRACE_PREFIX, $TRACE_ENABLED);
-$urban_age_section = ($toplevel_ancestor == 94 or $post->ID == 1798) ?  true : false;
-echo var_trace($post->ID, $TRACE_PREFIX, $TRACE_ENABLED);*/
-?>
 <ul id="organizer-logos">
   <li>
     <a href="http://www.lse.ac.uk/" target="_blank">
