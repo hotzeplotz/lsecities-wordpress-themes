a1c10bc079eedfa0b87b8a68cd47473eb0f70f24
andrea rota <a.rota@lse.ac.uk>
2012-05-08 14:05:33 +0100

    do not display sidenav in vacancies section

--- lsecities-2012/nav.php
+++ lsecities-2012/nav.php
@@ -30,7 +30,7 @@ elseif($current_post_id == 94) : // /ua/ (Urban Age frontpage)
   get_template_part('snippet-organizers');
 elseif($current_post_id == 309 or in_array(309, get_post_ancestors($current_post_id)) or ($pods_toplevel_ancestor == 309)) : // /publications (the whole Publications section) or individual Article pod items
   get_template_part('nav', 'publications');
-elseif($current_post_id == 489): // /ua/award/
+elseif($current_post_id == 489 or in_array(1890, get_post_ancestors($current_post_id))): // /ua/award/ or /about/collaboration-opportunities/
   get_template_part('nav', 'empty');
 elseif(check_parent_conference(191) or check_parent_conference(229) or check_parent_conference(250) or check_parent_conference(268) or check_parent_conference(211) or check_parent_conference(284) or check_parent_conference(286) or check_parent_conference(106) or check_parent_conference(381) or check_parent_conference(391) or check_parent_conference(577) or check_parent_conference(1388)):
   get_template_part('nav', 'conferences');
