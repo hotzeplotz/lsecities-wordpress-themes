cf6b123919437c67c3436a8928bc5749a284bf3a
andrea rota <a.rota@lse.ac.uk>
2012-05-25 11:06:02 +0100

    removed uservoice widget

--- lsecities-2012/footer.php
+++ lsecities-2012/footer.php
@@ -108,12 +108,6 @@
             $('input:radio[name=group[8245]]')[0].checked = true;
           }
         });
-        var uvOptions = {};
-        (function() {
-          var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
-          uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/6VLoqLrgtZvOGnGp2aYwXQ.js';
-          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
-        })();
       //]]>
 </script>
 </body>
