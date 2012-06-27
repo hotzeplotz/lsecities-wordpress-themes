e934588904965966f76f04414c3974e636bf7a84
andrea rota <a.rota@lse.ac.uk>
2012-06-27 17:03:32 +0100

    syntax

--- lsecities-2012/nav-whoswho.php
+++ lsecities-2012/nav-whoswho.php
@@ -3,7 +3,7 @@ $TRACE_ENABLED = is_user_logged_in();
 $TRACE_PREFIX = 'nav.php -- ';
 $current_post_id = $post->ID;
 $people_list = get_post_meta($post->ID, 'people_list', true);
-if($people_list) {
+if($people_list):
 ?>
 <nav id="whoswho-side-toc">
 <?php echo generate_list($people_list, 'summary'); ?>
