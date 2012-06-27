387eff9514f9524748a5a9090ab0a95ee215a46e
andrea rota <a.rota@lse.ac.uk>
2012-06-27 17:00:52 +0100

    cleaned up nav code

--- lsecities-2012/nav-whoswho.php
+++ lsecities-2012/nav-whoswho.php
@@ -2,8 +2,10 @@
 $TRACE_ENABLED = is_user_logged_in();
 $TRACE_PREFIX = 'nav.php -- ';
 $current_post_id = $post->ID;
+$people_list = get_post_meta($post->ID, 'people_list', true);
+if($people_list) {
 ?>
-
 <nav id="whoswho-side-toc">
-<?php echo generate_list('lsecities-staff', 'summary'); ?>
+<?php echo generate_list($people_list, 'summary'); ?>
 </nav>
+<?php endif; ?>
