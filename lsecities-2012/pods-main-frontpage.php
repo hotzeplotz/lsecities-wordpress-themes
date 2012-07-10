82f70b646f0e2dd86f154636c4142c836c3896cf
andrea rota <a.rota@lse.ac.uk>
2012-07-10 10:23:25 +0100

    added support for direct link to research project page

--- lsecities-2012/pods-main-frontpage.php
+++ lsecities-2012/pods-main-frontpage.php
@@ -90,7 +90,7 @@ function compose_slide($column_spans, $tiles) {
       if($tile->get_field('target_event.slug')) {
         $target_uri = '/media/objects/events/' . $tile->get_field('target_event.slug');
       } elseif($tile->get_field('target_research_project')) {
-        $target_uri = honor_ssl_for_attachments($tile->get_field('target_research_project.guid'));
+        $target_uri = '/objects/research-projects/' . $tile->get_field('target_research_project.slug');
       } elseif($tile->get_field('target_uri')) {
         $target_uri = $tile->get_field('target_uri');
       } elseif($tile->get_field('target_page.guid')) {
