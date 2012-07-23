c613badd3ed06f0f8c2b551b6f20fcd3ae9bf0ae
andrea rota <a.rota@lse.ac.uk>
2012-07-23 15:56:52 +0100

    refactored switch code

--- lsecities-2012/nav.php
+++ lsecities-2012/nav.php
@@ -47,25 +47,20 @@ else :
 endif;
 ?>
 
-
-            <?php switch(get_post_meta(get_the_ID(), 'list_pods', true)):
-              case 'research_projects':
-                $HIDE_CURRENT_PROJECTS = true;
-                $HIDE_PAST_PROJECTS = true;
-                $BASE_URI = PODS_BASEURI_RESEARCH_PROJECTS;
-                $IN_CONTENT_AREA = false;
-            ?>
-            <?php get_template_part( 'nav', 'research' ); ?>                
-            <?php break;
-              case 'past_research_projects':
-                $HIDE_PAST_PROJECTS = true;
-                $HIDE_CURRENT_PROJECTS = true;
-                $IN_CONTENT_AREA = false;
-                $BASE_URI = PODS_BASEURI_RESEARCH_PROJECTS;
-            ?>
-            <?php get_template_part( 'nav', 'research' ); ?>              
-            <?php break; ?>
-            <?php endswitch; ?>
+<?php
+if(get_post_meta(get_the_ID(), 'list_pods', true) == 'research_projects') {
+  $HIDE_CURRENT_PROJECTS = true;
+  $HIDE_PAST_PROJECTS = true;
+  $BASE_URI = PODS_BASEURI_RESEARCH_PROJECTS;
+  $IN_CONTENT_AREA = false;
+  get_template_part('nav', 'research');
+} elseif(get_post_meta(get_the_ID(), 'list_pods', true) == 'past_research_projects') {
+  $HIDE_PAST_PROJECTS = true;
+  $HIDE_CURRENT_PROJECTS = true;
+  $IN_CONTENT_AREA = false;
+  $BASE_URI = PODS_BASEURI_RESEARCH_PROJECTS;
+  get_template_part('nav', 'research');
+} ?>
             
             
   <div id="mailing-list-subscription">
