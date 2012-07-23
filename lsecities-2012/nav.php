2353fa53cbbe6272f4da7b6968178e9e339f3786
andrea rota <a.rota@lse.ac.uk>
2012-07-23 12:24:39 +0100

    signup box in sidebar

--- lsecities-2012/nav.php
+++ lsecities-2012/nav.php
@@ -43,4 +43,26 @@ else :
   get_template_part('nav', 'generic');
 endif;
 ?>
+
+  <div id="mailing-list-subscription">
+    <h1>Subscribe to LSE Cities updates</h1>
+    <form method="post" action="http://urban-age.us4.list-manage1.com/subscribe/post" id="mailchimp-form">
+      <input type="hidden" value="6a19b1b794ce991fff919b68d" name="u" />
+      <input type="hidden" value="1f3b65491d" name="id" />
+      <label for="MERGE0">email address <em>*</em></label>
+      <input type="email" value="" name="MERGE0" id="MERGE0" />
+      <label for="MERGE1">first name <em>*</em></label>
+      <input type="text" value="" name="MERGE1" id="MERGE1" />
+      <label for="MERGE2">last name <em>*</em></label>
+      <input type="text" value="" name="MERGE2" id="MERGE2" />
+      <input type="submit" />
+    </form>
+  </div>
+  <script>
+    //<![CDATA[
+    jQuery(document).ready(function($) {
+      $('#mailing-list-subscription > h1 > a').click(function() {$('#mailchimp-form').toggle()});
+      }));
+    //]]>
+  </script>
 </div>
