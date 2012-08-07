481dbe24ad251a38a0cabf5c9d5267155e3bee7c
andrea rota <a.rota@lse.ac.uk>
2012-08-07 16:13:05 +0100

    refactored metadata reading code

--- lsecities-2012/pods-events-frontpage.php
+++ lsecities-2012/pods-events-frontpage.php
@@ -129,13 +129,14 @@ $META_media_attributions = array();
 // if this is an event, grab the image URI from the Pod
 if(!$is_conference) {
   $featured_image_uri = honor_ssl_for_attachments($pod->get_field('heading_image.guid'));
+  $attachment_ID = $pod->get_field('heading_image.ID');
   echo var_trace($pod->get_field('heading_image'), $TRACE_PREFIX . ': heading_image', $TRACE_ENABLED);
-  $attachment_metadata = wp_get_attachment_metadata($pod->get_field('heading_image.ID'));
+  $attachment_metadata = wp_get_attachment_metadata($attachment_ID);
   echo var_trace($attachment_metadata, $TRACE_PREFIX . ': attachment_metadata', $TRACE_ENABLED);
   array_push($META_media_attributions, array(
     'title' => $pod->get_field('heading_image.name'),
-    'uri' => $attachment_metadata['AttributionURI'],
-    'author' => $attachment_metadata['AttributionName']
+    'uri' => get_post_meta($attachment_ID, '_AttributionURI', true),
+    'author' => get_post_meta($attachment_ID, '_AttributionName', true),
   ));
 }
 
