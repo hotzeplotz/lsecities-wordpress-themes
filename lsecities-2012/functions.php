e855ef496a39bd0fabaf1e90ac35ae68b5675e23
andrea rota <a.rota@lse.ac.uk>
2012-08-08 15:13:16 +0100

    logging to file when user is logged in

--- lsecities-2012/functions.php
+++ lsecities-2012/functions.php
@@ -6,6 +6,15 @@ define(PODS_BASEURI_CONFERENCES, '/media/objects/conferences');
 define(PODS_BASEURI_EVENTS, '/media/objects/events');
 define(PODS_BASEURI_RESEARCH_PROJECTS, '/objects/research-projects');
 
+// log php errors
+define(LSECITIES2012_LOG_FILE, '/srv/web/wordpress/www/tmp/lsecities-2012.log');
+
+if(is_user_logged_in()) {
+  @ini_set('log_errors', 'On'); // enable or disable php error logging (use 'On' or 'Off')
+  @ini_set('display_errors', 'Off'); // enable or disable public display of errors (use 'On' or 'Off')
+  @ini_set('error_log', LSECITIES2012_LOG_FILE); // path to server-writable log file
+}
+
 include_once('inc/asset_pipeline.php');
 // define assets to load
 $json_assets =
